ff66f163c9dea1b059b68301640e7d3c89cece33
islamhussein1911 <eslam20054@eng.bnu.edu.eg>
2025-11-20 23:42:06 +0200

    update

---
+++ android/app/src/main/kotlin/com/example/mostaqel1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mostaqel1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:mostaqel1/app.dart';
+
+void main() {
+  runApp(MyApp());
+}
